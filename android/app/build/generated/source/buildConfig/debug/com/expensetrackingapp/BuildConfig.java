4562d297c56edcaf7dc3353c6844a5390d89710a
Shivani <shivanialwal@gmail.com>
2021-04-09 12:32:16 +0530

    Basic Project setup

---
+++ android/app/build/generated/source/buildConfig/debug/com/expensetrackingapp/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.expensetrackingapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.expensetrackingapp";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
